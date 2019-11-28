c820a0292a9928da79997c4acb723b02d371ecce
AlexRod <alejandro.rod.vazquez@gmail.com>
2019-11-28 00:17:28 -0600

    feat(configs):add plugin configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,42 @@
  */
 
 module.exports = {
-  /* Your site config here */
   siteMetadata: {
+    title: 'PWA/contentful example',
     someData: 'Im the data'
   },
   plugins: [
+    // sass
     'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet'
-  ],
+    // material-ui Design system
+    'gatsby-theme-material-ui',
+    // head handling
+    'gatsby-plugin-react-helmet',
+    // offline suppport
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: { },
+    },
+    // web manifest
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'GatsbyJS',
+        short_name: 'GatsbyJS',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#452475',
+        display: 'standalone',
+        icon: 'src/images/icon.png',
+      },
+    },
+    // contentful pulling content
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    }
+  ]
 }
